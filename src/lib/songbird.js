45756e28c2b8d40fd3d72c9fc8ff5c9317bde180
Joshua Philpott <jphil529@gmail.com>
2019-08-04 21:23:46 -0600

    refresh token

--- src/lib/songbird.js
+++ src/lib/songbird.js
@@ -3,16 +3,36 @@ import Cookies from 'js-cookie'
 
 const spotifyApiBaseUrl = `${process.env.REACT_APP_API_URL}/api/spotify`
 
+const setSpotifyAccessToken = (access_token, expires_in) => {
+  // set the expires date
+  const expires = new Date()
+  expires.setSeconds(expires.getSeconds() + expires_in)
+
+  Cookies.set('spotify_access_token', access_token, {
+    expires
+  })
+}
+
 const fetchSpotifyAccessToken = async (code, state) => {
   const response = await axios.post(`${spotifyApiBaseUrl}/getAccessToken`, {
     code,
     state
   })
+  const { access_token, expires_in, refresh_token } = response.data
 
-  Cookies.set('spotify_access_token', response.data.access_token, {
-    maxAge: response.data.expires_in * 1000
+  setSpotifyAccessToken(access_token, expires_in)
+  Cookies.set('spotify_refresh_token', refresh_token)
+
+  return response.data
+}
+
+const refreshSpotifyToken = async refresh_token => {
+  const response = await axios.post(`${spotifyApiBaseUrl}/refreshToken`, {
+    refresh_token
   })
-  Cookies.set('spotify_refresh_token', response.data.refresh_token)
+
+  const { access_token, expires_in } = response.data
+  setSpotifyAccessToken(access_token, expires_in)
 
   return response.data
 }
@@ -22,4 +42,8 @@ const fetchSpotifyAuthUri = async () => {
   return response.data
 }
 
-export default { fetchSpotifyAccessToken, fetchSpotifyAuthUri }
+export default {
+  fetchSpotifyAccessToken,
+  fetchSpotifyAuthUri,
+  refreshSpotifyToken
+}
