205f587507a5ea20f818e902b35d5cce58a7efa4
Joshua Philpott <jphil529@gmail.com>
2019-08-17 08:12:31 -0600

    dot refactor

---
+++ src/components/Dot.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export default styled.section`
+  height: 6px;
+  width: 6px;
+  border-radius: 50%;
+  margin-right: 5px;
+  background-color: ${props => props.color};
+`
