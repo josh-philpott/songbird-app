28a73cf6a2f2a1fba487b7b803f313df3517412f
Joshua Philpott <jphil529@gmail.com>
2019-10-23 10:27:17 -0600

    On Air Avatar

--- src/components/room/player/OnAirAvatar.js
+++ src/components/room/player/OnAirAvatar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import styled from 'styled-components'
 
 import Avatar from '../../design-system/Avatar'
@@ -15,13 +15,34 @@ const OuterCircle = styled.section`
 
   margin-right: 10px;
 
-  background-image: linear-gradient(-45deg, #0250c5 0%, #d43f8d 100%);
-  background-image: linear-gradient(-45deg, #0250c5 0%, #d43f8d 100%);
+  background-image: linear-gradient(
+    ${props => props.degree},
+    #0250c5 0%,
+    #d43f8d 100%
+  );
+  background-image: linear-gradient(
+    ${props => props.degree},
+    #0250c5 0%,
+    #d43f8d 100%
+  );
 `
 
 function OnAirAvatar(props) {
+  const [degree, setDegree] = useState(0)
+  const degreeRef = useRef(degree)
+  degreeRef.current = degree
+
+  function rotateGradient() {
+    degreeRef.current = degreeRef.current >= 359 ? 0 : degreeRef.current + 2
+    setDegree(degreeRef.current)
+  }
+
+  useEffect(() => {
+    setInterval(rotateGradient, 10)
+  }, [])
+
   return (
-    <OuterCircle>
+    <OuterCircle degree={degree + 'deg'}>
       <Avatar
         size='xxl'
         source='https://scontent.xx.fbcdn.net/v/t1.0-1/p200x200/33748780_10155366585941366_7866216603870822400_n.jpg?_nc_cat=111&_nc_oc=AQn6ZmoQyySKHlPBE4T1VAsNeArBer8VXaUuGzb5vTh5oQ5M7nyf4rCMjIbGSvOBTkE&_nc_ht=scontent.xx&oh=25e31615788e90b5ef1b8644be637633&oe=5E28225E'
